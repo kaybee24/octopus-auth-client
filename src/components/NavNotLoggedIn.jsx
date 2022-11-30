919d5fab58ffcb1f15baa0b7eb6c192f52c2b1f4
samin <fatehsamin@gmail.com>
2022-11-30 13:30:10 +0100

    burger changes

--- src/components/NavNotLoggedIn.jsx
+++ src/components/NavNotLoggedIn.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Burger from './Burger'
 
 const NavNotLoggedIn = () => {
     return (
-        <nav>
+        <nav className='relative'>
             <ul className='flex justify-center font-openSans space-x-8'>
                 NOT LOGGED IN
                 <li><Link to="/">Home</Link></li>
             </ul>
+            <Burger/>
         </nav>
     )
 }
