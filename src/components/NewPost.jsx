24b1a0ee124fb2855e5bb33dff4f8a72f569db2a
‘Katrin <katrin.baier77@gmail.com>
2022-12-06 12:00:01 +0100

    disable new post button

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import { useCallback } from "react";
 
 const NewPost = ({setPosts}) => {
+  const [disabled, setDisabled] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
   const handleSubmit = useCallback((event) => {
     event.preventDefault();
     const form = event.target;
     const formData = new FormData(form);
+    setDisabled(true);
+  
     var object = {};
     formData.forEach(function (value, key) {
       object[key] = value;
@@ -34,8 +43,20 @@ const NewPost = ({setPosts}) => {
     <div className="container w-full max-w-7xl px-4 mt-6">
       <h2 className='text-4xl uppercase font-paytoneOne text-offwhite-900'>What&apos;s on your mind?</h2>
       <form className="w-full flex flex-col py-4 font-openSans" onSubmit={handleSubmit}>
-        <textarea className="p-5 my-2 bg-grey-100 outline-ocean-700 max-w-[70rem]" name="message" type="text" placeholder="Tell us what is on your mind" rows="4"></textarea>
-        <button className='bg-coral-500 hover:bg-coral-900 max-w-xs py-3 my-6 rounded font-OpenSans font-bold'>Create New Post</button>
+        <textarea 
+          className="p-5 my-2 bg-grey-100 outline-ocean-700 max-w-[70rem]" 
+          name="message" 
+          type="text" 
+          placeholder="Tell us what is on your mind" 
+          rows="4"
+          onChange={handleMessageChange}
+          value={message}>
+        </textarea>
+        <button 
+         disabled={!message} 
+        className='bg-coral-500 hover:bg-coral-900 disabled:opacity-70 disabled:pointer-events-none max-w-xs py-3 my-6 rounded font-OpenSans font-bold'>
+          Create New Post
+          </button>
       </form>
     </div>
   );
